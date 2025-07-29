a85258690423476b366f108df1288425b1dff7be
egor90909091 <n778hr@mail.ru>
2025-07-29 20:46:42 +0500

    DB, prisma, auth-StartPoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')             // Заголовок документации
+    .setDescription('Описание вашего API')    // Описание
+    .setVersion('1.0')                         // Версия API
+    .addTag('example')                         // Тег для группировки эндпоинтов (необязательно)
+    .build();
+
+  // Создание документа Swagger
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Подключение Swagger UI к пути '/api'
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
